82af552869016b372033e6fa30788ff4e7f4ee23
gyenesmatyas <matyas.gyenes@student.kdg.be>
2021-05-07 10:47:46 +0100

    commit

--- js/javascript.js
+++ js/javascript.js
@@ -2,7 +2,8 @@
 
 // lange versie
 $(document).ready(function(){
-    $('#btnFetch').click(function fetch(){
+    
+    $('#btnCoindesk').click(function fetch(){
         $('#loader').show();
         $.ajax({
             url:"https://api.coindesk.com/v1/bpi/currentprice.json",
@@ -10,11 +11,26 @@ $(document).ready(function(){
             dataType:"JSON",
             data: JSON.stringify({ }),
             success:function(data){    
-                $(".messages").html("");            
-                $(".messages").append("<li>De prijs in USD: " + data['bpi']['USD']['rate'] + "</li>");
-                $(".messages").append("<li>De prijs in GBP: " + data['bpi']['GBP']['rate'] + "</li>");
-                $(".messages").append("<li>De prijs in EUR: " + data['bpi']['EUR']['rate'] + "</li>");
-                $(".messages").append("<li>Deze prijzen zijn laatst geupdate om: " + data['time']['updated']+ "</li>");
+                $(".coindeskMessages").html("");            
+                $(".coindeskMessages").append("<li>De prijs in USD: " + data['bpi']['USD']['rate'] + "</li>");
+                $(".coindeskMessages").append("<li>De prijs in GBP: " + data['bpi']['GBP']['rate'] + "</li>");
+                $(".coindeskMessages").append("<li>De prijs in EUR: " + data['bpi']['EUR']['rate'] + "</li>");
+                $(".coindeskMessages").append("<li>Deze prijzen zijn laatst geupdate om: " + data['time']['updated']+ "</li>");
+            }
+        });
+        $('#loader').hide();
+    });
+    
+    $('#btnCoinAPI').click(function fetch(){
+        $('#loader').show();
+        $.ajax({
+            url:"https://rest.coinapi.io/v1/assets/eth?apikey=0A8976A2-B68B-4CF1-8E71-A614A73D7EB0",
+            tyope:"GET",
+            dataType:"JSON",
+            data: JSON.stringify({ }),
+            success:function(data){    
+                $(".coinAPIMessages").html("");
+                $(".coinAPIMessages").append("<li>ETH price in USD: " + data["0"].price_usd+ "</li>");
             }
         });
         $('#loader').hide();
